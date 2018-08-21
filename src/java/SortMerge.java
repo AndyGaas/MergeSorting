7f2da68d09240ffa5e287e1ef09accc90e928739
andygaas <andyhaas@mail.ru>
2018-08-22 00:02:38 +0700

    refactoring

--- src/java/SortMerge.java
+++ src/java/SortMerge.java
@@ -1,11 +1,7 @@
-package Java;
+package java;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class SortMerge {
     public static void main(String[] args) throws IOException {
